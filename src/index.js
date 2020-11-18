eadf35964b4049baa21334a06a6565472183c6db
Hieu Nguyen Trong <hieuth1997@gmail.com>
2020-11-18 13:33:00 +0700

    add handlebars

--- src/index.js
+++ src/index.js
@@ -11,11 +11,15 @@ app.use(morgan("combined"));
 app.get("/", (req, res) => {
     res.render("home");
 });
+app.get("/news", (req, res) => {
+    res.render("news");
+});
 //template engine
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 //set home directory
 app.set("views", path.join(__dirname, "resources/views"));
+app.set("news", path.join(__dirname, "resources/news"));
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
