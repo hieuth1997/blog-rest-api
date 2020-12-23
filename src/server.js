1b31d2e077b031efb48929998eb24e19fae31e22
Hieu Nguyen Trong <hieuth1997@gmail.com>
2020-12-24 01:04:41 +0700

    build init server project

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
-const express = require("express");
+const express = require('express');
+import morgan from 'morgan';
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
+
+app.use(morgan('combined'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
