728709536e5251d60f68d9968c9a9e40ad79d97d
Hieu Nguyen Trong <hieuth1997@gmail.com>
2021-01-03 20:27:09 +0700

    connect database

---
+++ src/database/database.service.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+const mongoUri = process.env.DB_URI || 'mongodb://192.168.1.9:27017';
+const option = {
+  useNewUrlParser: true,
+  autoIndex: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+};
+const connect = () => {
+  mongoose.connect(mongoUri, option, (err) => {
+    console.log(err || 'database is connected');
+  });
+};
+export default { connect };
