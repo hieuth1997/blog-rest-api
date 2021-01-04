9c4324ce93aa4582e6c02f043f6c0fbbe84fee09
Hieu Nguyen Trong <hieuth1997@gmail.com>
2021-01-04 23:30:47 +0700

    edit auth module

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,10 +1,21 @@
 import userService from '../users/users.service';
 import BaseResponse from '../commons/helpers/baseResponse';
 import BaseError from '../commons/helpers/baseError';
+import { get } from 'mongoose';
 export default {
+  async get(req, res, next) {
+    const test = {
+      islogin: true,
+    };
+    return new BaseResponse({
+      statusCode: 200,
+      data: { message: 'create success user.', test },
+    }).return(res);
+  },
   async register(req, res, next) {
     try {
       const newUser = req.body;
+      console.log(newUser);
       const user = await userService.createOne(newUser);
       if (user) {
         return new BaseResponse({
--- src/auth/auth.route.js
+++ src/auth/auth.route.js
@@ -3,6 +3,7 @@ import authController from './auth.controller';
 import { isValid } from '../commons/validation/validator';
 import { register, login } from './auth.validation';
 const router = express.Router();
+router.get('/register', authController.get);
 router.post('/register', isValid(register), authController.register);
 router.post('/login', isValid(login), authController.logIn);
 export { router as authRouter };
