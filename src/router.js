fbe37e3b0e43271f772a5d788a1c47e1876ddea7
Hieu Nguyen Trong <hieuth1997@gmail.com>
2021-01-27 13:09:55 +0700

    add news route

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ const router = express.Router();
 import { authRouter } from './auth/auth.route';
 import { authorize } from './auth/auth.middleware';
 import { userRouter } from './users/users.route';
+
 router.use('/auth', authRouter);
 router.use('/user', authorize, userRouter);
+router.use('/news', newsRouter);
 export default router;
